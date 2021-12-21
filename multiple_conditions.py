398669eea8ce23414a530c325cb99be06ae1dac0
mario-galindo <mgalindo9410@gmail.com>
2021-12-20 22:17:35 -0600

    working with mutiples condition

--- multiple_conditions.py
+++ multiple_conditions.py
@@ -1,10 +1,14 @@
-province = input("What province do you live in? ")
-tax = 0
+country = input("What country do you live in? ")
 
-if province == "Alberta" or province == "Nanavut":
-    tax = 0.05
-elif province == "Ontario":
-    tax = 0.13
+if country.lower() == "canada":
+    province = input("What province/state do you live in? ")
+    if province in ("Alberta", "Nanavut"):
+        tax = 0.05
+    elif province == "Ontario":
+        tax = 0.13
+    else:
+        tax = 0.15
 else:
-    tax = 0.15
+    tax = 0.0
+
 print(tax)
