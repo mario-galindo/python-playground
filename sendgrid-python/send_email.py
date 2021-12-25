c8b90fbafbaa6bdbbbf7e527b82ea76cfdea7a53
mario-galindo <mgalindo9410@gmail.com>
2021-12-25 00:45:47 -0600

    sendgrid api key moved to python env variables

---
+++ sendgrid-python/send_email.py
@@ -0,0 +1,19 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, template_id
+
+message = Mail(
+    from_email="notify@developersindustry.com",
+    to_emails="mgalindo@devinhn.com",
+    subject="Sending with Twilio SendGrid!",
+    html_content="<strong>Sent with Python</strong>",
+)
+
+try:
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e)
---
+++ sendgrid-python/send_email_template_dynamic.py
@@ -0,0 +1,22 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, template_id
+
+message = Mail(
+    from_email="notify@developersindustry.com", to_emails="mgalindo@devinhn.com"
+)
+
+message.template_id = "d-1ede0b26870e4be89b88515f214517fc"
+message.dynamic_template_data = {
+    "subject": "Ganaste!",
+    "name": "Miguel",
+}
+
+try:
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e)
